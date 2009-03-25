76e9b3d0081523188906755d33800dd07f6768ba
Sitaram Chamarty <sitaramc@gmail.com>
2009-03-25 09:02:16 +0530

    cvs2git: reversal of recommendation
    exhaustive testing (30+ tags on a project with 5 years of CVS history in it)
    reveals that the -inline stuff is more correct -- this is a complete reversal
    from what I thought earlier.

--- tools/cvs2git.sh
+++ tools/cvs2git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $0
+less $0
 exit
 
 # -------- sorry, not implemented yet
@@ -25,28 +25,39 @@ exit
 # - in there, create an options file from the original, like shown in the diff
 #   at the end of this file (change accordingly of course; this diff only
 #   shows where you should make the changes, not what)
-#   (Avoid using the "test-data/main-cvsrepos/cvs2svn-git-inline.options" file
-#   as a starting point.  I thought that would be a single step process, but
-#   it still needed 2 steps, and the intermediate files were 3X larger.
-#   However, the output seems to be closer to the original CVS in terms of the
-#   $Id stuff.  YMMV)
+
+#   !!!!! IMPORTANT WARNING !!!!!
+
+#   DO NOT USE the "test-data/main-cvsrepos/cvs2svn-git.options" file as a
+#   starting point.  Though the intermediate files were about 3X smaller when
+#   I used it, there were lots of inaccuracies w.r.t the $id type stuff, and
+#   for some older tags whole files were missing, compared to the
+#   corresponding CVS checkout.  The "-inline" version seems to work fine; no
+#   errors on any of the 30 or so tags I checked on a project that had about 5
+#   years of work in CVS.
 
 # - run
 #       ./cvs2svn --options=my.c2soptions
-# - when it completes, check the 'cvs2svn-tmp' directory for 2 files
+# - when it completes, check the 'cvs2svn-tmp' directory for a rather large
+#   file called git-dump.dat
 
 # part 2
 
 # - make an empty directory, cd to it, git init, then run
+#       cat ~-/cvs2svn-tmp/git-dump.dat        | git fast-import
+# - if you used the non-inline options file, or wanted to test that as well,
+#   there would be *two* files in the cvs2svn-tmp directory, and the command
+#   would now be:
 #       cat ~-/cvs2svn-tmp/git-{blob,dump}.dat | git fast-import
 
 # that should do...
+
 cat <<EOF >/dev/null
-diff --git 1/test-data/main-cvsrepos/cvs2svn-git.options 2/my.c2soptions
-index 46510eb..fb26b6f 100644
---- 1/test-data/main-cvsrepos/cvs2svn-git.options
-+++ 2/my.c2soptions
-@@ -41,7 +41,7 @@ ctx.cross_branch_commits = False
+diff --git 1/test-data/main-cvsrepos/cvs2svn-git-inline.options 2/my.c2soptions.inline
+index 635f9cd..a5a4018 100644
+--- 1/test-data/main-cvsrepos/cvs2svn-git-inline.options
++++ 2/my.c2soptions.inline
+@@ -39,7 +39,7 @@ ctx.cross_branch_commits = False
  # record the original author (for example, the creation of a branch).
  # This should be a simple (unix-style) username, but it can be
  # translated into a git-style name by the author_transforms map.
@@ -55,21 +66,21 @@ index 46510eb..fb26b6f 100644
  
  # CVS uses unix login names as author names whereas git requires
  # author names to be of the form "foo <bar>".  The default is to set
-@@ -61,7 +61,7 @@ author_transforms={
+@@ -59,7 +59,7 @@ author_transforms={
  
      # This one will be used for commits for which CVS doesn't record
      # the original author, as explained above.
 -    'cvs2svn' : ('cvs2svn', 'admin@example.com'),
-+    'someone' : ('someone', 'someone@your.company.com'),
++    'someone' : ('someone', 'someone@my.company.com'),
      }
  
  # This is the main option that causes cvs2svn to output to git rather
-@@ -119,7 +119,7 @@ run_options.add_project(
+@@ -115,7 +115,7 @@ run_options.add_project(
      # The path to the part of the CVS repository (*not* a CVS working
      # copy) that should be converted.  This may be a subdirectory
      # (i.e., a module) within a larger CVS repository.
 -    r'test-data/main-cvsrepos',
-+    r'path/to/cvsroot/module',
++    r'../cvsroot/myproj',
  
      # See cvs2svn-example.options for more documention about symbol
      # transforms that can be set using this option.
